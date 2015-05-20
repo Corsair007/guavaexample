5aca2b3115c1d67e751af47dcf1afe9862b90215
abel.cui <chc340121@126.com>
2015-05-20 16:14:21 +0800

    first commit

---
+++ src/main/java/com/yeahmobi/rundemo/guavaexample/App.java
@@ -0,0 +1,13 @@
+package com.yeahmobi.rundemo.guavaexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
